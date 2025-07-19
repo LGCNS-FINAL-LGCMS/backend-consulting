780be4849615b4dacdd09d7ddbd1dc53d2eb1cb2
yoonho <nam2660433@gmail.com>
2025-07-19 10:20:58 +0900

    [LB-24] feat: Json 임베딩

---
+++ src/main/java/com/lgcms/consulting/domain/vectorDB/ocr/CustomJsonReader.java
@@ -0,0 +1,23 @@
+package com.lgcms.consulting.domain.vectorDB.ocr;
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.JsonReader;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CustomJsonReader {
+    private final Resource resource;
+
+    public CustomJsonReader(@Value("classpath:review.json") Resource resource) {
+        this.resource = resource;
+    }
+
+    public List<Document> loadJsonAsDocuments() {
+        JsonReader jsonReader = new JsonReader(this.resource, "description", "content");
+        return jsonReader.get();
+    }
+}
