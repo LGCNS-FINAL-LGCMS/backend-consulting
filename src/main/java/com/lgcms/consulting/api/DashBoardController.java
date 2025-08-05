d12909bfc1f5031242cd478c42cdd61ac213d2ec
yoonho <nam2660433@gmail.com>
2025-08-05 15:32:01 +0900

    [LB-258] refactor: path 수정

--- src/main/java/com/lgcms/consulting/api/DashBoardController.java
+++ src/main/java/com/lgcms/consulting/api/DashBoardController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/dashboard")
+@RequestMapping("/api/consulting/dashboard")
 @RestController
 @RequiredArgsConstructor
 public class DashBoardController {
