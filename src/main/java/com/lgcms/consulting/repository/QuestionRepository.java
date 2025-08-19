f55aa3d2d936facb25966e413378dea90ebcb1c3
yoonho <nam2660433@gmail.com>
2025-08-19 20:08:10 +0900

    [LB-246] feat: Tool 구성

--- src/main/java/com/lgcms/consulting/repository/QuestionRepository.java
+++ src/main/java/com/lgcms/consulting/repository/QuestionRepository.java
@@ -2,6 +2,19 @@ package com.lgcms.consulting.repository;
 
 import com.lgcms.consulting.domain.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    @Query(value = """
+            SELECT q.title
+            FROM question q JOIN lecture l ON l.id = q.lecture_id
+            WHERE l.member_id = :memberId
+            LIMIT 30
+            """, nativeQuery = true)
+    List<String> findByLecturerId(
+            @Param("memberId") Long lecturerId
+    );
 }
--- src/main/java/com/lgcms/consulting/repository/ReviewRepository.java
+++ src/main/java/com/lgcms/consulting/repository/ReviewRepository.java
@@ -1,7 +1,22 @@
 package com.lgcms.consulting.repository;
 
 import com.lgcms.consulting.domain.Review;
+import com.lgcms.consulting.dto.projection.ReportDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    @Query(value = """
+            SELECT r.content, r.star, r.details, r.etc, l.title
+            FROM review r JOIN lecture l ON r.lecture_id = l.id
+            WHERE l.member_id = :memberId
+            ORDER BY r.updated_at DESC
+            LIMIT 30
+            """, nativeQuery = true)
+    List<ReportDTO.ReviewData> findByLecturerId(
+            @Param("memberId") Long lecturerId
+    );
 }
