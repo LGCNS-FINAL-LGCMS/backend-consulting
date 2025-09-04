de5cb0615c0186e9492ec799c1680ba54d012e39
yoonho <nam2660433@gmail.com>
2025-09-05 00:50:12 +0900

    [LB-420] feat: 어노테이션에서 인터셉터로 변경 및 임베딩 토큰 추적 추가

--- src/main/java/com/lgcms/consulting/api/ReportController.java
+++ src/main/java/com/lgcms/consulting/api/ReportController.java
@@ -5,10 +5,7 @@ import com.lgcms.consulting.dto.response.report.ReportResponse;
 import com.lgcms.consulting.service.ai.BedrockService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/lecturer/consulting/report")
 @RestController
@@ -16,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReportController {
     private final BedrockService bedrockService;
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<BaseResponse<ReportResponse>> getReport(@RequestHeader("X-USER-ID") Long id) {
         return ResponseEntity.ok(BaseResponse.ok(bedrockService.getReport(id)));
     }
--- src/main/java/com/lgcms/consulting/common/aspect/TokenMetricsAspect.java
+++ src/main/java/com/lgcms/consulting/common/aspect/TokenMetricsAspect.java
@@ -1,12 +1,12 @@
 package com.lgcms.consulting.common.aspect;
 
-import com.lgcms.consulting.common.annotation.TokenMetrics;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+//import org.springframework.ai.bedrock.titan.api.TitanEmbeddingBedrockApi;
 import org.springframework.ai.chat.metadata.Usage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.stereotype.Component;
@@ -17,20 +17,36 @@ import org.springframework.stereotype.Component;
 public class TokenMetricsAspect {
     private final MeterRegistry meterRegistry;
 
-    @Around("@annotation(tokenMetrics)")
-    public Object recordTokenMetrics(ProceedingJoinPoint joinPoint, TokenMetrics tokenMetrics) throws Throwable {
+    @Around("execution(* org.springframework.ai.chat.model.ChatModel.call(..))")
+    public Object recordTokenMetrics(ProceedingJoinPoint joinPoint) throws Throwable {
         Object result = joinPoint.proceed();
 
         if(result instanceof ChatResponse chatResponse) {
             Usage usage = chatResponse.getMetadata().getUsage();
             Integer totalTokens = usage.getTotalTokens();
 
-            Counter.builder(tokenMetrics.value())
-                    .description(tokenMetrics.description())
+            Counter.builder("ai.tokens")
+                    .description("LLM token usage")
                     .register(meterRegistry)
                     .increment(totalTokens);
         }
 
         return result;
     }
+
+//    @Around("execution(* org.springframework.ai.bedrock.titan.api.TitanEmbeddingBedrockApi.embedding(..))")
+//    public Object recordEmbeddingTokenMetrics(ProceedingJoinPoint joinPoint) throws Throwable {
+//        Object result = joinPoint.proceed();
+//
+//        if(result instanceof TitanEmbeddingBedrockApi.TitanEmbeddingResponse titanEmbeddingResponse) {
+//            Integer token = titanEmbeddingResponse.inputTextTokenCount();
+//
+//            Counter.builder("ai.tokens")
+//                    .description("LLM token usage")
+//                    .register(meterRegistry)
+//                    .increment(token);
+//        }
+//
+//        return result;
+//    }
 }
